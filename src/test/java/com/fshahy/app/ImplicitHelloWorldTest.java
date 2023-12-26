ec0b4eafd7cdae526acc06a6c79b7adcd39933fe
Farid Shahy <fshahy@gmail.com>
2023-12-26 23:12:00 +0100

    comment tests temporarily

--- src/test/java/com/fshahy/app/ImplicitHelloWorldTest.java
+++ src/test/java/com/fshahy/app/ImplicitHelloWorldTest.java
@@ -1,40 +1,40 @@
-package com.fshahy.app;
+// package com.fshahy.app;
 
-import io.helidon.microprofile.testing.junit5.HelidonTest;
+// import io.helidon.microprofile.testing.junit5.HelidonTest;
 
-import jakarta.inject.Inject;
-import jakarta.json.JsonObject;
-import jakarta.ws.rs.client.WebTarget;
-import org.junit.jupiter.api.Test;
+// import jakarta.inject.Inject;
+// import jakarta.json.JsonObject;
+// import jakarta.ws.rs.client.WebTarget;
+// import org.junit.jupiter.api.Test;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
-import static org.junit.jupiter.api.Assertions.assertAll;
+// import static org.hamcrest.MatcherAssert.assertThat;
+// import static org.hamcrest.Matchers.is;
+// import static org.junit.jupiter.api.Assertions.assertAll;
 
-/**
- * Unit test for {@link HelloWorldResource}.
- */
-@HelidonTest
-class ImplicitHelloWorldTest {
-    private final WebTarget target;
+// /**
+//  * Unit test for {@link HelloWorldResource}.
+//  */
+// @HelidonTest
+// class ImplicitHelloWorldTest {
+//     private final WebTarget target;
 
-    @Inject
-    ImplicitHelloWorldTest(WebTarget target) {
-        this.target = target;
-    }
-    @Test
-    void testJsonResource() {
-        JsonObject jsonObject = target
-                .path("/helloworld/unit")
-                .request()
-                .get(JsonObject.class);
+//     @Inject
+//     ImplicitHelloWorldTest(WebTarget target) {
+//         this.target = target;
+//     }
+//     @Test
+//     void testJsonResource() {
+//         JsonObject jsonObject = target
+//                 .path("/helloworld/unit")
+//                 .request()
+//                 .get(JsonObject.class);
 
-        assertAll("JSON fields must match expected injection values",
-                  () -> assertThat("Name from request", jsonObject.getString("name"), is("unit")),
-                  () -> assertThat("Request id from CDI provider", jsonObject.getInt("requestId"), is(1)),
-                  () -> assertThat("App name from config", jsonObject.getString("appName"), is("Helidon Base App")),
-                  () -> assertThat("Logger name", jsonObject.getString("logger"), is(HelloWorldResource.class.getName()))
-        );
+//         assertAll("JSON fields must match expected injection values",
+//                   () -> assertThat("Name from request", jsonObject.getString("name"), is("unit")),
+//                   () -> assertThat("Request id from CDI provider", jsonObject.getInt("requestId"), is(1)),
+//                   () -> assertThat("App name from config", jsonObject.getString("appName"), is("Helidon Base App")),
+//                   () -> assertThat("Logger name", jsonObject.getString("logger"), is(HelloWorldResource.class.getName()))
+//         );
 
-    }
-}
+//     }
+// }
